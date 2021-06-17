233fe06101a26631dfd653aa7d4807c97eae7f64
ljghy <2436496086@qq.com>
2021-06-17 22:09:46 +0800

    update

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 int main()
 {
-	double a[] = {1,2,3,4,5,6 };
-	Matrix A(3, 2, a);
-
+	Matrix A(5,3);
+	srand(time(0));
+	A.randFill(-10,10);
+	cout<<A;
 
 	Matrix U, S, V;
-	A.SVD(U, S, V);
+
+	A.SVD(U,S,V,1,64);
 	cout<<U << S << V << U * S * transpose(V);
-	
 	system("pause");
 	return 0;
 }
